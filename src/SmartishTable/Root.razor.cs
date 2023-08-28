d74feac08b57f0089805d7c641733d0a60a3aa07
Jeremy Smith <jsmith@mca.net>
2023-08-28 15:39:19 -0400

    Updating crud functions.

--- src/SmartishTable/Root.razor.cs
+++ src/SmartishTable/Root.razor.cs
@@ -13,282 +13,308 @@ namespace SmartishTable;
 
 public partial class Root<SmartishTItem> : IDisposable
 {
-    [Parameter]
-    public List<SmartishTItem> SafeList { get; set; } = default!;
+  [Parameter]
+  public List<SmartishTItem> SafeList { get; set; } = default!;
 
-    /// <summary>
-    /// Contains the filtered/sorted displaylist.  Only contains current displayed page.
-    /// </summary>
-    public List<SmartishTItem>? DisplayList { get; internal set; }
+  /// <summary>
+  /// Contains the filtered/sorted displaylist.  Only contains current displayed page.
+  /// </summary>
+  public List<SmartishTItem>? DisplayList { get; internal set; }
 
-    [Parameter]
-    public RenderFragment ChildContent { get; set; } = default!;
+  [Parameter]
+  public RenderFragment ChildContent { get; set; } = default!;
 
-    [Parameter]
-    public string SortAscendingCss { get; set; } = "smartish-table-sort-asc";
+  [Parameter]
+  public string SortAscendingCss { get; set; } = "smartish-table-sort-asc";
 
-    [Parameter]
-    public string SortDescendingCss { get; set; } = "smartish-table-sort-desc";
+  [Parameter]
+  public string SortDescendingCss { get; set; } = "smartish-table-sort-desc";
 
-    [Parameter]
-    public string NoSortCss { get; set; } = "";
+  [Parameter]
+  public string NoSortCss { get; set; } = "";
 
-    /// <summary>
-    /// true if you want to use the SortAscendingCss, SortDescendingCss and NoSortCss values
-    /// </summary>
-    [Parameter]
-    public bool UseSortCss { get; set; } = true;
+  /// <summary>
+  /// true if you want to use the SortAscendingCss, SortDescendingCss and NoSortCss values
+  /// </summary>
+  [Parameter]
+  public bool UseSortCss { get; set; } = true;
 
-    /// <summary>
-    /// Default: th
-    /// </summary>
-    [Parameter]
-    public string HeaderTag { get; set; } = "th";
+  /// <summary>
+  /// Default: th
+  /// </summary>
+  [Parameter]
+  public string HeaderTag { get; set; } = "th";
 
-    /// <summary>
-    /// Event to listen to when data is updated and sends current configuration
-    /// </summary>
-    [Parameter]
-    public EventCallback<SmartishTableSettings> OnDataUpdated { get; set; }
+  /// <summary>
+  /// Event to listen to when data is updated and sends current configuration
+  /// </summary>
+  [Parameter]
+  public EventCallback<SmartishTableSettings> OnDataUpdated { get; set; }
 
-    /// <summary>
-    /// Default:  1
-    /// </summary>
-    [Range(1, int.MaxValue)]
-    [Parameter]
-    public int MaxNumberOfSorts { get; set; } = 1;
+  /// <summary>
+  /// Default:  1
+  /// </summary>
+  [Range(1, int.MaxValue)]
+  [Parameter]
+  public int MaxNumberOfSorts { get; set; } = 1;
 
-    /// <summary>
-    /// Initial settings
-    /// </summary>
-    [Parameter]
-    public SmartishTableSettings? InitialSettings { get; set; }
+  /// <summary>
+  /// Initial settings
+  /// </summary>
+  [Parameter]
+  public SmartishTableSettings? InitialSettings { get; set; }
 
-    internal ColumnSortCollection<SmartishTItem> ColumnSorts = default!;
-    internal ColumnFilterCollection<SmartishTItem> ColumnFilters = default!;
-    internal Paginator Paginator = default!;
-    private bool maxNumberOfSortsDecreased = false;
-    private bool disposedValue;
-    private readonly Type rootType = typeof(Root<SmartishTItem>);
-    private static readonly string[] ReloadTriggerParameters = new string[]
-    {
+  internal ColumnSortCollection<SmartishTItem> ColumnSorts = default!;
+  internal ColumnFilterCollection<SmartishTItem> ColumnFilters = default!;
+  internal Paginator Paginator = default!;
+  private bool maxNumberOfSortsDecreased = false;
+  private bool disposedValue;
+  private readonly Type rootType = typeof(Root<SmartishTItem>);
+  private static readonly string[] ReloadTriggerParameters = new string[]
+  {
         nameof(SafeList),
         nameof(MaxNumberOfSorts),
+  };
+
+  protected override void OnInitialized()
+  {
+    Paginator = new Paginator()
+    {
+      page = 1
     };
+    Paginator.PropertyChanged += Paginator_PropertyChanged;
+  }
 
-    protected override void OnInitialized()
+  public override async Task SetParametersAsync(ParameterView parameters)
+  {
+    var shouldReload = false;
+    var p = parameters.ToDictionary();
+
+    foreach (var item in ReloadTriggerParameters)
     {
-        Paginator = new Paginator()
+      if (p.ContainsKey(item))
+      {
+        var newValue = p[item]?.GetHashCode();
+        var oldValue = rootType.GetProperty(item)!.GetValue(this)?.GetHashCode();
+
+        shouldReload = newValue != oldValue;
+
+        if (item == nameof(MaxNumberOfSorts))
         {
-            page = 1
-        };
-        Paginator.PropertyChanged += Paginator_PropertyChanged;
-    }
-
-    public override async Task SetParametersAsync(ParameterView parameters)
-    {
-        var shouldReload = false;
-        var p = parameters.ToDictionary();
-
-        foreach (var item in ReloadTriggerParameters)
-        {
-            if (p.ContainsKey(item))
-            {
-                var newValue = p[item]?.GetHashCode();
-                var oldValue = rootType.GetProperty(item)!.GetValue(this)?.GetHashCode();
-
-                shouldReload = newValue != oldValue;
-
-                if (item == nameof(MaxNumberOfSorts))
-                {
-                    maxNumberOfSortsDecreased = MaxNumberOfSorts > (int)p[item];
-                }
-            }
-
-            if (shouldReload)
-            {
-                break;
-            }
+          maxNumberOfSortsDecreased = MaxNumberOfSorts > (int)p[item];
         }
+      }
 
-        await base.SetParametersAsync(parameters);
-
-        if (shouldReload)
-            await Refresh();
+      if (shouldReload)
+      {
+        break;
+      }
     }
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
+    await base.SetParametersAsync(parameters);
+
+    if (shouldReload)
+      await Refresh();
+  }
+
+  protected override async Task OnAfterRenderAsync(bool firstRender)
+  {
+    if (InitialSettings != null && firstRender)
     {
-        if (InitialSettings != null && firstRender)
-        {
-            await SetSettings(InitialSettings, true);
-        }
+      await SetSettings(InitialSettings, true);
     }
+  }
 
-    private async void Paginator_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+  private async void Paginator_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+  {
+    if (Paginator.PaginatorPropertiesChangedList.Contains(e.PropertyName))
+      await Refresh();
+  }
+
+  /// <summary>
+  /// Gets the settings for smartish table
+  /// </summary>
+  /// <returns><see cref="SmartishTableSettings"/></returns>
+  public SmartishTableSettings GetSettings()
+  {
+    return new SmartishTableSettings()
     {
-        if (Paginator.PaginatorPropertiesChangedList.Contains(e.PropertyName))
-            await Refresh();
-    }
+      Page = Paginator.Page,
+      PageSize = Paginator.PageSize,
+      ColumnSorts = ColumnSorts?.GetSortSettings()
+    };
+  }
 
-    /// <summary>
-    /// Gets the settings for smartish table
-    /// </summary>
-    /// <returns><see cref="SmartishTableSettings"/></returns>
-    public SmartishTableSettings GetSettings()
+  /// <summary>
+  /// Sets smartish table settings and refreshes
+  /// </summary>
+  /// <param name="settings"><see cref="SmartishTableSettings"/></param>
+  public async Task SetSettings(SmartishTableSettings settings)
+  {
+    await SetSettings(settings, true);
+  }
+
+  private async Task SetSettings(SmartishTableSettings settings, bool refresh)
+  {
+    if (settings == null)
+      return;
+
+    if (ColumnSorts != null)
+      ColumnSorts.SetSortSettings(MaxNumberOfSorts, settings.ColumnSorts);
+
+    if (settings.PageSize.HasValue)
+      Paginator.pageSize = settings.PageSize.Value;
+
+    Paginator.page = settings.Page ?? 1;
+
+    if (refresh)
+      await Refresh();
+  }
+
+  private List<SmartishTItem>? GetData()
+  {
+    if (SafeList == null)
+      return null;
+
+    var query = SafeList.AsQueryable();
+
+    if (ColumnFilters != null)
+      query = ColumnFilters.SetFilters(query);
+
+    if (ColumnSorts != null)
     {
-        return new SmartishTableSettings()
-        {
-            Page = Paginator.Page,
-            PageSize = Paginator.PageSize,
-            ColumnSorts = ColumnSorts?.GetSortSettings()
-        };
+      if (maxNumberOfSortsDecreased)
+      {
+        ColumnSorts.RemoveHighestSortOrders(MaxNumberOfSorts);
+        maxNumberOfSortsDecreased = false;
+      }
+      query = ColumnSorts.SetOrderBys(query);
     }
 
-    /// <summary>
-    /// Sets smartish table settings and refreshes
-    /// </summary>
-    /// <param name="settings"><see cref="SmartishTableSettings"/></param>
-    public async Task SetSettings(SmartishTableSettings settings)
+    Paginator.Count = query.Count();
+
+    if (Paginator.PageSize.HasValue)
+      query = query.Skip(Paginator.PageSize.Value * (Paginator.Page - 1)).Take(Paginator.PageSize.Value);
+
+    return query.ToList();
+  }
+
+  /// <summary>
+  /// Filters must be added here to function properly
+  /// </summary>
+  /// <param name="filterComponent"><see cref="IFilter{SmartishTItem}"/></param>
+  public void AddFilterComponent(IFilter<SmartishTItem> filterComponent)
+  {
+    if (ColumnFilters == null)
+      ColumnFilters = new ColumnFilterCollection<SmartishTItem>();
+    ColumnFilters.Add(filterComponent);
+  }
+
+  /// <summary>
+  /// Refreshes smartish table by getting data again
+  /// </summary>
+  /// <param name="resetPaging">resets the page to page 1</param>
+  public async Task Refresh(bool resetPaging = false)
+  {
+    if (resetPaging)
+      Paginator.Page = 1;
+
+    DisplayList = GetData();
+
+    if (OnDataUpdated.HasDelegate)
+      await OnDataUpdated.InvokeAsync(GetSettings());
+
+    StateHasChanged();
+  }
+
+  /// <summary>
+  /// Adds an item safe list
+  /// </summary>
+  /// <param name="item"><see cref="SmartishTItem"/></param>
+  /// <param name="refresh">refresh smartish table after performing action</param>
+  public Task Add(SmartishTItem item, bool refresh = true)
+  {
+    SafeList.Add(item);
+    if (refresh)
+      return Refresh();
+
+    return Task.CompletedTask;
+  }
+
+  /// <summary>
+  /// Adds an item range to the safe list
+  /// </summary>
+  /// <param name="items"><see cref="List{SmartishTItem}"/></param>
+  /// <param name="refresh">refresh smartish table after performing action</param>
+  public Task AddRange(IEnumerable<SmartishTItem> items, bool refresh = true)
+  {
+    SafeList.AddRange(items);
+    if (refresh)
+      return Refresh();
+
+    return Task.CompletedTask;
+  }
+
+  /// <summary>
+  /// Updates an item at specified index
+  /// </summary>
+  /// <param name="index">index of the displayed item (index is provided by the repeater context)</param>
+  /// <param name="item"><see cref="SmartishTItem"/></param>
+  /// <param name="refresh">refresh smartish table after performing action</param>
+  public Task UpdateAt(int index, SmartishTItem item, bool refresh = true)
+  {
+    var dataIndex = SafeList.IndexOf(DisplayList[index]);
+    SafeList[dataIndex] = item;
+    if (refresh)
+      return Refresh();
+
+    return Task.CompletedTask;
+  }
+
+  /// <summary>
+  /// Removes the item at specified index
+  /// </summary>
+  /// <param name="index">index of the displayed item (index is provided by the repeater context)</param>
+  /// <param name="refresh">refresh smartish table after performing action</param>
+  public Task RemoveAt(int index, bool refresh = true)
+  {
+    var item = DisplayList[index];
+    SafeList.Remove(item);
+    if (refresh)
+      return Refresh();
+
+    return Task.CompletedTask;
+  }
+
+  /// <summary>
+  /// Get the item at specified index
+  /// </summary>
+  /// <param name="index">index of the displayed item (index is provided by the repeater context)</param>
+  /// <returns><see cref="SmartishTItem"/></returns>
+  public SmartishTItem GetAt(int index)
+  {
+    return DisplayList[index];
+  }
+
+  protected virtual void Dispose(bool disposing)
+  {
+    if (!disposedValue)
     {
-        await SetSettings(settings, true);
+      if (disposing)
+      {
+        Paginator.PropertyChanged -= Paginator_PropertyChanged;
+      }
+
+      disposedValue = true;
     }
+  }
 
-    private async Task SetSettings(SmartishTableSettings settings, bool refresh)
-    {
-        if (settings == null)
-            return;
-
-        if (ColumnSorts != null)
-            ColumnSorts.SetSortSettings(MaxNumberOfSorts, settings.ColumnSorts);
-
-        if (settings.PageSize.HasValue)
-            Paginator.pageSize = settings.PageSize.Value;
-
-        Paginator.page = settings.Page ?? 1;
-
-        if (refresh)
-            await Refresh();
-    }
-
-    private List<SmartishTItem>? GetData()
-    {
-        if (SafeList == null)
-            return null;
-
-        var query = SafeList.AsQueryable();
-
-        if (ColumnFilters != null)
-            query = ColumnFilters.SetFilters(query);
-
-        if (ColumnSorts != null)
-        {
-            if (maxNumberOfSortsDecreased)
-            {
-                ColumnSorts.RemoveHighestSortOrders(MaxNumberOfSorts);
-                maxNumberOfSortsDecreased = false;
-            }
-            query = ColumnSorts.SetOrderBys(query);
-        }
-
-        Paginator.Count = query.Count();
-
-        if (Paginator.PageSize.HasValue)
-            query = query.Skip(Paginator.PageSize.Value * (Paginator.Page - 1)).Take(Paginator.PageSize.Value);
-
-        return query.ToList();
-    }
-
-    /// <summary>
-    /// Filters must be added here to function properly
-    /// </summary>
-    /// <param name="filterComponent"><see cref="IFilter{SmartishTItem}"/></param>
-    public void AddFilterComponent(IFilter<SmartishTItem> filterComponent)
-    {
-        if (ColumnFilters == null)
-            ColumnFilters = new ColumnFilterCollection<SmartishTItem>();
-        ColumnFilters.Add(filterComponent);
-    }
-
-    /// <summary>
-    /// Refreshes smartish table by getting data again
-    /// </summary>
-    /// <param name="resetPaging">resets the page to page 1</param>
-    public async Task Refresh(bool resetPaging = false)
-    {
-        if (resetPaging)
-            Paginator.Page = 1;
-
-        DisplayList = GetData();
-
-        if (OnDataUpdated.HasDelegate)
-            await OnDataUpdated.InvokeAsync(GetSettings());
-
-        StateHasChanged();
-    }
-
-    /// <summary>
-    /// Adds an item safe list
-    /// </summary>
-    /// <param name="item"><see cref="SmartishTItem"/></param>
-    public Task Add(SmartishTItem item)
-    {
-        SafeList.Add(item);
-        return Refresh();
-    }
-
-    /// <summary>
-    /// Updates an item at specified index
-    /// </summary>
-    /// <param name="index">index of the displayed item (index is provided by the repeater context)</param>
-    /// <param name="item"><see cref="SmartishTItem"/></param>
-    public Task UpdateAt(int index, SmartishTItem item)
-    {
-        var dataIndex = SafeList.IndexOf(DisplayList[index]);
-        SafeList[dataIndex] = item;
-        return Refresh();
-    }
-
-    /// <summary>
-    /// Removes the item at specified index
-    /// </summary>
-    /// <param name="index">index of the displayed item (index is provided by the repeater context)</param>
-    public Task RemoveAt(int index)
-    {
-        var item = DisplayList[index];
-        SafeList.Remove(item);
-        return Refresh();
-    }
-
-    /// <summary>
-    /// Get the item at specified index
-    /// </summary>
-    /// <param name="index">index of the displayed item (index is provided by the repeater context)</param>
-    /// <returns><see cref="SmartishTItem"/></returns>
-    public SmartishTItem GetAt(int index)
-    {
-        return DisplayList[index];
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!disposedValue)
-        {
-            if (disposing)
-            {
-                Paginator.PropertyChanged -= Paginator_PropertyChanged;
-            }
-
-            disposedValue = true;
-        }
-    }
-
-    /// <summary>
-    /// Disposes SmartishTable
-    /// </summary>
-    public void Dispose()
-    {
-        Dispose(disposing: true);
-        GC.SuppressFinalize(this);
-    }
+  /// <summary>
+  /// Disposes SmartishTable
+  /// </summary>
+  public void Dispose()
+  {
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
 }
