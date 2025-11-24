fb79890d8c73cacf0635bda5083137e8dff0b8b5
Jeremy Smith <jsmith@mca.net>
2025-11-24 11:05:35 -0500

    clean up

--- src/SmartishTable.Samples/Client/Program.cs
+++ src/SmartishTable.Samples/Client/Program.cs
@@ -1,7 +1,6 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
